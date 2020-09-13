4b75938af1bdf819df0c10aa582895ecfeafe2f8
samkitjain <samkit1913058@akgec.ac.in>
2020-09-13 23:10:45 +0530

    Added ListView for Scrolling of Category

--- lib/appUI.dart
+++ lib/appUI.dart
@@ -50,21 +50,51 @@ class AppUI extends StatelessWidget {
                   ),
                   Container(
                     height: 120,
-                    child: ListView(
+                    child: ListView.separated(
+                      separatorBuilder: (context, index) => SizedBox(
+                        height: 100,
+                        width: 20,
+                      ),
+                      itemCount: 11,
+                      // ignore: missing_return
+                      itemBuilder: (context, index) {
+                        switch (index) {
+                          case 0:
+                            return cat(catImg: 'pizza', txt: 'Pizza');
+                            break;
+                          case 1:
+                            return cat(catImg: 'hamburger', txt: 'Burger');
+                            break;
+                          case 2:
+                            return cat(catImg: 'sandwich', txt: 'Sandwich');
+                            break;
+                          case 3:
+                            return cat(catImg: 'bread', txt: 'Breads');
+                            break;
+                          case 4:
+                            return cat(catImg: 'coffee', txt: 'Coffee');
+                            break;
+                          case 5:
+                            return cat(catImg: 'hot-dog', txt: 'Hot Dog');
+                            break;
+                          case 6:
+                            return cat(catImg: 'pie', txt: 'Pie');
+                            break;
+                          case 7:
+                            return cat(catImg: 'muffin', txt: 'Muffin');
+                            break;
+                          case 8:
+                            return cat(catImg: 'spaguetti', txt: 'Noodles');
+                            break;
+                          case 9:
+                            return cat(catImg: 'soda', txt: 'Drinks');
+                            break;
+                          case 10:
+                            return cat(catImg: 'grid', txt: 'More');
+                            break;
+                        }
+                      },
                       scrollDirection: Axis.horizontal,
-                      children: [
-                        cat(catImg: 'pizza', txt: 'Pizza'),
-                        cat(catImg: 'hamburger', txt: 'Burger'),
-                        cat(catImg: 'sandwich', txt: 'Sandwich'),
-                        cat(catImg: 'bread', txt: 'Breads'),
-                        cat(catImg: 'coffee', txt: 'Coffee'),
-                        cat(catImg: 'hot-dog', txt: 'Hot Dog'),
-                        cat(catImg: 'pie', txt: 'Pie'),
-                        cat(catImg: 'muffin', txt: 'Muffin'),
-                        cat(catImg: 'spaguetti', txt: 'Noodles'),
-                        cat(catImg: 'soda', txt: 'Drinks'),
-                        cat(catImg: 'grid', txt: 'More'),
-                      ],
                     ),
                   ),
                 ],
@@ -82,3 +112,16 @@ class AppUI extends StatelessWidget {
     );
   }
 }
+//  Row(children: [
+
+//                           ,
+//                           ,
+//                           ,
+//                           ,
+//                           ,
+//                           ,
+//                           ,
+//                           ,
+//                           ,
+//
+//                         ]);
